24449d0f177c3627ea65300f2ed1e473343e6097
haxceb <haxcebmalik@yahoo.com>
2022-05-23 09:54:00 +0500

    some changes

--- src/Pages/EventListing/event-listing.test.js
+++ src/Pages/EventListing/event-listing.test.js
@@ -23,6 +23,34 @@ describe.only('Test Event Listing Page', () => {
     const backButton = screen.getByRole('back-btn')
     expect(backButton).toBeInTheDocument()
   })
+
+  test('if artist card is in the document', () => {
+    const testUrl = `${window.location}/events/Amir`
+    window.history.pushState({}, 'Test Title', testUrl)
+    render(<EventListingPage />)
+
+    axiosMock.get.mockResolvedValueOnce({
+      data: {
+        artist_optin_show_phone_number: false,
+        facebook_page_url: 'http://www.facebook.com/580571262037389',
+        id: '20357',
+        image_url: 'https://photos.bandsintown.com/large/8231335.jpeg',
+        links: [{ type: 'website', url: 'http://www.amirofficiel.com' }],
+        mbid: '5dd0c8e8-68aa-4c4e-b686-3f77c574602a',
+        name: 'Amir',
+        options: { display_listen_unit: false },
+        support_url: '',
+        thumb_url: 'https://photos.bandsintown.com/thumb/8231335.jpeg',
+        tracker_count: 20080,
+        tracking: [],
+        upcoming_event_count: 36,
+        url: 'https://www.bandsintown.com/a/20357?came_from=267&app_id=abc'
+      }
+    })
+
+    // const backButton = screen.getByRole('back-btn')
+    // expect(backButton).toBeInTheDocument()
+  })
   // test('search input value', async () => {
   //   const testUrl = `${window.location}/events/Amir`
   //   window.history.pushState({}, 'Test Title', testUrl)
--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  get: jest.fn().mockResolvedValue({ data: {} })
+  get: jest.fn().mockResolvedValue({ data: {} }),
+  getEvents: jest.fn().mockResolvedValue({ data: {} })
 }
