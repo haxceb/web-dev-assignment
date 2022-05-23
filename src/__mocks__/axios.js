eb620bc0ac80b4b030ae50bfe26ac99d27fe5bc4
haxceb <haxcebmalik@yahoo.com>
2022-05-23 10:02:07 +0500

    Updates

--- src/__mocks__/axios.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default {
-  get: jest.fn().mockResolvedValue({ data: {} }),
-  getEvents: jest.fn().mockResolvedValue({ data: {} })
-}
--- src/apis/endpoints.js
+++
@@ -1,8 +0,0 @@
-const apiDomain = 'https://rest.bandsintown.com'
-
-export const ENDPOINTS = {
-  GET: {
-    getArtists: artistName => `${apiDomain}/artists/${artistName}`,
-    getEvents: artistName => `${apiDomain}/artists/${artistName}/events`
-  }
-}
